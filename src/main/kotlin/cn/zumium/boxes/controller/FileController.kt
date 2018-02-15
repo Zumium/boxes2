e2e40debc885f1ebb5346fa1d457d1529ef38a95
Zumium <martin007323@gmail.com>
2018-02-15 22:07:34 +0800

    bug fix

--- src/main/kotlin/cn/zumium/boxes/controller/FileController.kt
+++ src/main/kotlin/cn/zumium/boxes/controller/FileController.kt
@@ -18,57 +18,73 @@ class FileController(override val kodein: Kodein) : Iface, KodeinAware {
 
     override fun add(boxId: Long, innerPath: String, outerPath: String, addBy: AddBy) {
         reportException("adding file or dir to box") {
-            val box = transaction { dbManager.box.getBox(boxId) }
-            fsManager.file.add(box.name, innerPath, outerPath, addBy)
+            transaction {
+                val box = dbManager.box.getBox(boxId)
+                fsManager.file.add(box.name, innerPath, outerPath, addBy)
+            }
         }
     }
 
     override fun fetch(boxId: Long, innerPath: String, outerPath: String, fetchBy: FetchBy) {
         reportException("fetching file or dir from box") {
-            val box = transaction { dbManager.box.getBox(boxId) }
-            fsManager.file.fetch(box.name, innerPath, outerPath, fetchBy)
+            transaction {
+                val box = dbManager.box.getBox(boxId)
+                fsManager.file.fetch(box.name, innerPath, outerPath, fetchBy)
+            }
         }
     }
 
     override fun remove(boxId: Long, innerPath: String) {
         reportException("removing file or dir from box") {
-            val box = transaction { dbManager.box.getBox(boxId) }
-            fsManager.file.remove(box.name, innerPath)
+            transaction {
+                val box = dbManager.box.getBox(boxId)
+                fsManager.file.remove(box.name, innerPath)
+            }
         }
     }
 
     override fun ls(boxId: Long, innerDir: String): List<LsItem> = reportException("listing files and dirs") {
-        val box = transaction { dbManager.box.getBox(boxId) }
-        fsManager.file.ls(box.name, innerDir)
+        transaction {
+            val box = dbManager.box.getBox(boxId)
+            fsManager.file.ls(box.name, innerDir)
+        }
     }
 
     override fun move(srcBoxId: Long, srcInnerPath: String, dstBoxId: Long, dstInnerPath: String) {
         reportException("moving file or dir between boxes") {
-            val srcBox = transaction { dbManager.box.getBox(srcBoxId) }
-            val dstBox = transaction { dbManager.box.getBox(dstBoxId) }
-            fsManager.file.moveBetweenBox(srcBox.name, srcInnerPath, dstBox.name, dstInnerPath)
+            transaction {
+                val srcBox = dbManager.box.getBox(srcBoxId)
+                val dstBox = dbManager.box.getBox(dstBoxId)
+                fsManager.file.moveBetweenBox(srcBox.name, srcInnerPath, dstBox.name, dstInnerPath)
+            }
         }
     }
 
     override fun copy(srcBoxId: Long, srcInnerPath: String, dstBoxId: Long, dstInnerPath: String) {
         reportException("copying file or dir between boxes") {
-            val srcBox = transaction { dbManager.box.getBox(srcBoxId) }
-            val dstBox = transaction { dbManager.box.getBox(dstBoxId) }
-            fsManager.file.copyBetweenBox(srcBox.name, srcInnerPath, dstBox.name, dstInnerPath)
+            transaction {
+                val srcBox = dbManager.box.getBox(srcBoxId)
+                val dstBox = dbManager.box.getBox(dstBoxId)
+                fsManager.file.copyBetweenBox(srcBox.name, srcInnerPath, dstBox.name, dstInnerPath)
+            }
         }
     }
 
     override fun innerMove(boxId: Long, srcInnerPath: String, dstInnerPath: String) {
         reportException("moving file or dir inside box") {
-            val box = transaction { dbManager.box.getBox(boxId) }
-            fsManager.file.innerMove(box.name, srcInnerPath, dstInnerPath)
+            transaction {
+                val box = dbManager.box.getBox(boxId)
+                fsManager.file.innerMove(box.name, srcInnerPath, dstInnerPath)
+            }
         }
     }
 
     override fun innerCopy(boxId: Long, srcInnerPath: String, dstInnerPath: String) {
         reportException("copying file or dir inside box") {
-            val box = transaction { dbManager.box.getBox(boxId) }
-            fsManager.file.innerCopy(box.name, srcInnerPath, dstInnerPath)
+            transaction {
+                val box = dbManager.box.getBox(boxId)
+                fsManager.file.innerCopy(box.name, srcInnerPath, dstInnerPath)
+            }
         }
     }
 }
